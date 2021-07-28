ce3ae3dd4ff2bb69f771128ccbda4d951914e8cf
Halimat <aliev.djamal.2000@gmail.com>
2021-07-28 22:40:29 +0300

    "SignUp" add main logic

--- app/src/main/java/com/example/guidanceitschool/SignUpActivity.java
+++ app/src/main/java/com/example/guidanceitschool/SignUpActivity.java
@@ -1,14 +1,33 @@
 package com.example.guidanceitschool;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText emailEditText;
+    private EditText nameEditText;
+    private EditText passwordEditText;
+    private Button signUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        initViews();
+    }
+
+    private void initViews() {
+        emailEditText = findViewById(R.id.email_edit_text_sign_up);
+        nameEditText = findViewById(R.id.name_edit_text_sign_up);
+        passwordEditText = findViewById(R.id.password_edit_text_sign_up);
+        signUpButton = findViewById(R.id.sign_up_button);
+
+        signUpButton.setOnClickListener(v -> {
+
+        });
     }
 }
