e5a68a3458433608298f91158ff07c6e484069a3
Halimat <aliev.djamal.2000@gmail.com>
2021-07-28 22:27:49 +0300

    init commit

---
+++ app/src/main/java/com/example/guidanceitschool/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.guidanceitschool;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button signUpButton;
+    private TextView signInTextView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        initViews();
+    }
+
+    private void initViews() {
+        signUpButton = findViewById(R.id.sign_up_button_welcome);
+        signInTextView = findViewById(R.id.sign_in_text_welcome);
+
+        signUpButton.setOnClickListener(v -> {
+            // TODO добавить переход на экран "SignUp"
+        });
+        signInTextView.setOnClickListener(v -> {
+            // TODO добавить переход на экран "SignIn"
+        });
+    }
+}
