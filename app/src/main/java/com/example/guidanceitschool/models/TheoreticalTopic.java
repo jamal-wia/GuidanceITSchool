4440cef0d5b811ca3e9de7654e6cac846073b901
Halimat <aliev.djamal.2000@gmail.com>
2021-08-18 12:12:42 +0300

    "Topic" create topic models

---
+++ app/src/main/java/com/example/guidanceitschool/models/TheoreticalTopic.java
@@ -0,0 +1,14 @@
+package com.example.guidanceitschool.models;
+
+public class TheoreticalTopic extends Topic {
+
+    private String text;
+
+    public TheoreticalTopic(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
---
+++ app/src/main/java/com/example/guidanceitschool/models/Topic.java
@@ -0,0 +1,6 @@
+package com.example.guidanceitschool.models;
+
+import java.io.Serializable;
+
+public abstract class Topic implements Serializable {
+}
